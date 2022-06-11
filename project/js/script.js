34edf6d2381c9891945bdfc1e94976c076c7c949
IlyaSoGood <87233953+IlyaSoGood@users.noreply.github.com>
2022-06-11 04:59:13 +0300

    First Practice JS on web-page

--- project/js/script.js
+++ project/js/script.js
@@ -23,4 +23,34 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
+// document.querySelector('.promo__adv').remove(); // УДАЛЯЕТ ВЕСЬ БЛОК
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+    //   movieList = document.querySelectorAll('.promo__interactive-item'); МОЙ СПОСОБ
+      movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+    item.remove();
+});
+
+genre.textContent = 'Драма';
+
+// poster.style.cssText = 'background: url("../img/bg.jpg") center center/cover no-repeat; background-position: top';
+// poster.style.background = 'url("img/bg.jpg") center top/cover no-repeat';  //ПРИМЕР
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+// МОЙ СПОСОБ: ПЕРЕЗАПИСЬ КАЖДОГО ЭЛЕМЕНТА ВЕРСТКИ ИЗ ЭЛЕМЕНТА МАССИВА.
+// movieList.forEach(function(item, i) {
+//     item.innerHTML = `${i + 1}. ${movieDB.movies.sort()[i]}<div class="delete"></div>`;
+// }); 
+// ИВАНА СПОСОБ: МАССИВ ФОРМИРУЕТ КАЖДЫЙ РАЗ СТРОКУ ВЕРСТКИ
+movieDB.movies.sort();
+movieList.innerHTML = "";
+movieDB.movies.forEach(function(film, i) {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1}. ${film}
+            <div class="delete"></div>
+        </li>`;
+});
 
